9a1b656620aa58ef82df30def61e31a29c3aea76
ansolvera <38958444+ansolvera@users.noreply.github.com>
2022-10-17 11:32:05 -0700

    Debug

--- page-resources.php
+++ page-resources.php
@@ -1,14 +1,8 @@
 <?php
 
-$args = array(
-    'post_type' => 'resources',
-    'post_status' => 'publish',
-    'perm' => 'readable',
-    'nopaging' => true,
-);
-
 $context = Timber::context();
-$context['team_members'] = new Timber\PostQuery( $args );
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
 
 Timber::render( array( 'pages/' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
 
